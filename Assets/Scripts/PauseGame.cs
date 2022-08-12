7a3b98f9064c483b4f4bf3966d06f0bfccb52ad7
Ashleigh Morris <ash.morris255@gmail.com>
2022-08-12 16:43:26 -0400

    WIP access child object in pause menu script

--- Assets/Scripts/PauseGame.cs
+++ Assets/Scripts/PauseGame.cs
@@ -7,10 +7,13 @@ public class PauseGame : MonoBehaviour
     public bool isPaused = false;
     public bool teleportMode = false;
 
+    private Transform locomotionSystem;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        locomotionSystem = transform.Find("Locomotion System");
+        Debug.Log(locomotionSystem.name);
     }
 
     // Update is called once per frame
