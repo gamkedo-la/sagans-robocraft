e5a9a2d22387cfe40c8851395b0fe31e616a068a
TheHaad <fmuntaz@gmail.com>
2022-08-14 13:57:54 -0400

    create audio manager; add to level object

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource audioSource;
+    
+    public void PlayAudio()
+    {
+        audioSource.Play();
+    }
+
+    public void PlayAudio(AudioClip audioClip, bool shouldLoop)
+    {
+        audioSource.Stop();
+        audioSource.loop = shouldLoop;
+        audioSource.clip = audioClip;
+        audioSource.Play();
+    }
+
+    public void StopAudio()
+    {
+        audioSource.Stop();
+    }
+
+    public void PauseAudio()
+    {
+        audioSource.Pause();
+    }
+
+}
