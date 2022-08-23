1a5ed28c44ea5791a4f617c327c598b75bf5897f
Ashleigh Morris <ash.morris255@gmail.com>
2022-08-23 12:09:36 -0400

    cont move, snap turn dis/enabled on bool

--- Assets/Scripts/PauseGame.cs
+++ Assets/Scripts/PauseGame.cs
@@ -2,26 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
-
 public class PauseGame : MonoBehaviour
 {
     public bool isPaused = false;
     public bool teleportMode = false;
 
     private Transform locomotionSystem;
-    //private ActionBasedContinuousMoveProvider contMotion;
     private ContinuousMoveProviderBase contMotion;
+    private SnapTurnProviderBase snapTurn;
+
 
     // Start is called before the first frame update
     void Start()
     {
+
         locomotionSystem = transform.Find("Locomotion System");
         contMotion = locomotionSystem.GetComponent<ContinuousMoveProviderBase>();
+        snapTurn = locomotionSystem.GetComponent<SnapTurnProviderBase>();
         
         if(teleportMode){
             contMotion.enabled = false;
+            snapTurn.enabled = true;
+        } else {
+            contMotion.enabled = true;
+            snapTurn.enabled = false;
         }
-        Debug.Log(contMotion.enabled);
+        //Debug.Log(contMotion.enabled);
+        Debug.Log(snapTurn.enabled);
 
     }
 
