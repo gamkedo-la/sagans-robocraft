4eec65e741ac14d6cdab230e4bf42f012a8d6167
Ashleigh Morris <ash.morris255@gmail.com>
2022-08-24 21:12:04 -0400

    accessed right hand controller in script

--- Assets/Scripts/PauseGame.cs
+++ Assets/Scripts/PauseGame.cs
@@ -8,6 +8,8 @@ public class PauseGame : MonoBehaviour
     public bool teleportMode = false;
 
     private Transform locomotionSystem;
+    private Transform cameraOffset;
+    private Transform rightHandController;
     private ContinuousMoveProviderBase contMotion;
     private SnapTurnProviderBase snapTurn;
 
@@ -19,6 +21,18 @@ public class PauseGame : MonoBehaviour
         locomotionSystem = transform.Find("Locomotion System");
         contMotion = locomotionSystem.GetComponent<ContinuousMoveProviderBase>();
         snapTurn = locomotionSystem.GetComponent<SnapTurnProviderBase>();
+
+        cameraOffset = transform.Find("Camera Offset");
+        rightHandController = cameraOffset.Find("RightHand Controller");
+
+        //rightHandController.AddComponent(typeof(XRDirectInteractor)) as XRDirectInteractor;
+        //idk why the below *has* to be assigned to a variable
+        //currently you need to either convert/find the gameObject attached to rightHandController
+        XRDirectInteractor dirInteractor = gameObject.AddComponent(typeof(XRDirectInteractor)) as XRDirectInteractor;
+
+
+        Debug.Log(cameraOffset);
+        Debug.Log(rightHandController);
         
         if(teleportMode){
             contMotion.enabled = false;
@@ -28,7 +42,7 @@ public class PauseGame : MonoBehaviour
             snapTurn.enabled = false;
         }
         //Debug.Log(contMotion.enabled);
-        Debug.Log(snapTurn.enabled);
+        //Debug.Log(snapTurn.enabled);
 
     }
 
