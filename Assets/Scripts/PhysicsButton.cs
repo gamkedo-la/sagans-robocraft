2b2c6d6db9ccca5260462b6185af70d248ab03f2
TheHaad <fmuntaz@gmail.com>
2022-08-28 14:22:20 -0400

    mainmenuvr scene - add functional physics button [wip]

--- Assets/Scripts/PhysicsButton.cs
+++ Assets/Scripts/PhysicsButton.cs
@@ -1,18 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PhysicsButton : MonoBehaviour
 {
+    // Ref:Physics Button Tutorial https://www.youtube.com/watch?v=HFNzVMi5MSQ
+
+    [SerializeField] private float threshold = .1f;
+    [SerializeField] private float deadZone = 0.025f;
+
+    private bool _isPressed;
+    private Vector3 _startPos;
+    private ConfigurableJoint _joint;
+
+    public UnityEvent onPressed, onReleased;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _startPos = transform.localPosition;
+        _joint = GetComponent<ConfigurableJoint>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!_isPressed && GetValue() + threshold >=1)
+        {
+            Pressed();
+        }
+
+        if (_isPressed && GetValue() - threshold <=0)
+        {
+            Released();
+        }
+    }
+
+    private float GetValue()
+    {
+        var value = Vector3.Distance(_startPos, transform.localPosition) / _joint.linearLimit.limit;
+
+        if (Mathf.Abs(value) < deadZone)
+        {
+            value = 0;
+        }
+
+        return Mathf.Clamp(value, -1f, 1f);
+    }
+
+    private void Pressed()
+    {
+        _isPressed = true;
+        Debug.Log("pressed");
+
+        onPressed.Invoke();
+    }
+
+    private void Released()
+    {
+        _isPressed = false;
+        onReleased.Invoke();
     }
 }
