de58542ada9048dba9e390c2fe4621828619cb54
Ashleigh Morris <ash.morris255@gmail.com>
2022-08-28 20:12:18 -0400

    WIP teleportation mode in MainScene

--- Assets/Scripts/PauseGame.cs
+++ Assets/Scripts/PauseGame.cs
@@ -11,36 +11,44 @@ public class PauseGame : MonoBehaviour
     private Transform cameraOffset;
     private Transform rightHandController;
     private GameObject rightHandControllerGameObject;
+    private TeleportationProvider teleProvider;
+
     private ContinuousMoveProviderBase contMotion;
     private SnapTurnProviderBase snapTurn;
+    private ContinuousTurnProviderBase contTurn;
 
 
     // Start is called before the first frame update
     void Start()
     {
         locomotionSystem = transform.Find("Locomotion System");
+        
+        teleProvider = locomotionSystem.GetComponent<TeleportationProvider>();
         contMotion = locomotionSystem.GetComponent<ContinuousMoveProviderBase>();
         snapTurn = locomotionSystem.GetComponent<SnapTurnProviderBase>();
+        contTurn = locomotionSystem.GetComponent<ContinuousTurnProviderBase>();
+        
 
         cameraOffset = transform.Find("Camera Offset");
         rightHandController = cameraOffset.Find("RightHand Controller");
         rightHandControllerGameObject = rightHandController.gameObject;
 
-        Debug.Log(cameraOffset);
         Debug.Log(rightHandController);
         
         if(teleportMode){
+            teleProvider.enabled = true;
             contMotion.enabled = false;
             snapTurn.enabled = true;
+            contTurn.enabled = false;
             XRRayInteractor rayInteractor = rightHandControllerGameObject.AddComponent(typeof(XRRayInteractor)) as XRRayInteractor;
 
         } else {
+            teleProvider.enabled = false;
             contMotion.enabled = true;
             snapTurn.enabled = false;
+            contTurn.enabled = true;
             XRDirectInteractor dirInteractor = rightHandControllerGameObject.AddComponent(typeof(XRDirectInteractor)) as XRDirectInteractor;
         }
-        //Debug.Log(contMotion.enabled);
-        //Debug.Log(snapTurn.enabled);
 
     }
 
